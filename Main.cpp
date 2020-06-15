01a8c85f60542ed0e2629c001298ee21f1119d26
nicolekwli <nicolekwli19@gmail.com>
2020-06-15 23:07:15 +0100

    main main set up

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,40 @@
 #include "Main.h"
 
+#define WIDTH 640
+#define HEIGHT 480
+
+DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+
 int main(int argc, char* argv[]){
+    SDL_Event event;
+    while(true){
+        if(window.pollForInputEvents(&event)) handleEvent(event);
+        update();
+        window.renderFrame();
+    }
+}
+
+// Function for performing animation (shifting artifacts or moving the camera)
+void update() {
+}
+
+void handleEvent(SDL_Event event)
+{
+  if(event.type == SDL_KEYDOWN) {
+    if(event.key.keysym.sym == SDLK_LEFT) cout << "LEFT" << endl;
+    else if(event.key.keysym.sym == SDLK_RIGHT) cout << "RIGHT" << endl;
+    else if(event.key.keysym.sym == SDLK_UP) cout << "UP" << endl;
+    else if(event.key.keysym.sym == SDLK_DOWN) cout << "DOWN" << endl;
+    else if(event.key.keysym.sym == SDLK_u){
+        // drawRandomTriangle();
+        cout << "U" << endl;
+    }
+    else if (event.key.keysym.sym == SDLK_f){
+        // drawFilledTriangle();
+    }    
+    else if(event.key.keysym.sym == SDLK_f){
+     // drawRandomFilledTriangle();
+    } 
+  }
+  else if(event.type == SDL_MOUSEBUTTONDOWN) cout << "MOUSE CLICKED" << endl;
 }
--- Main.h
+++ Main.h
@@ -1,4 +1,8 @@
 #include <glm/glm.hpp>
+#include "DrawingWindow.h"
 
 using namespace std;
 using namespace glm;
+
+void update();
+void handleEvent(SDL_Event event);
